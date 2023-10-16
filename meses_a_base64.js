ceae8dadb7a3d7441c9ef2135d99358478dcb5d7
ArtEze <passymas@gmail.com>
2023-10-15 22:05:58 -0300

    Update meses_a_base64.js

--- meses_a_base64.js
+++ meses_a_base64.js
@@ -8,6 +8,22 @@ function abc_faltan(nabc){
 	var g = [...nabc].map(x=>abc.indexOf(x)==-1?x:"").join("")
 	return f+g
 }
+function b64_a_bigint(b64){
+	var bi = BigInt("0x"+[...atob(b64)]
+		.map(x=>(256+x.charCodeAt()).toString(16).slice(1))
+		.join(""))
+	return bi
+}
+function bigint_a_cod(nabc,bi){
+	var arr = []
+	var base = BigInt(nabc.length)
+	while(bi>0){
+		arr.unshift( (10+ +(""+(bi%base))).toString(36) )
+		bi /= base
+	}
+	var s = arr.join("").toUpperCase()
+	return s
+}
 function ordenar_vocales(dabc){
 	var i = 0, j = 0, rabc = [], v = "AEIOU "
 	var abc = Array(26).fill("").map(x=> String.fromCharCode((97+j++)) ).join("").toUpperCase()
@@ -24,46 +40,30 @@ function ordenar_vocales(dabc){
 	rabc = rabc.join("").toUpperCase()
 	return rabc
 }
-function codificar(a,texto){
-	var nt = []
-	var may = texto.toUpperCase()
-	var rabc = ordenar_vocales(may)
-	var m = [...may].map(x=>nt.push(rabc.indexOf(x)))
-	var n = [...parseInt(a,3).toString(rabc.length-1)]
-		.map(x=>(10+parseInt(x,36)).toString(36))
-		.join("")
-		.toUpperCase()
-	var o = nt.map(x=> (10+x).toString(36) ).join("").toUpperCase()
-	var p = o + (10+rabc.indexOf(" ")).toString(36).toUpperCase() + n
-	return [rabc,n,p]
+function decodificar(nabc,cod){
+	var mabc = abc_faltan(nabc)
+	var sep = mabc.match(/[A-Z]/g).slice(-1)[0]
+	var dec = [...cod].map(x=>nabc[parseInt(x,36)-10]).join("")
+	return dec
 }
-function cod_a_bigint(x){
-	var base = BigInt([...new Set(x)].length)
-	var bi = 0n
-	for(var i in x){
-		bi *= base
-		bi += BigInt(parseInt(x[i],36)-10)
-	}
-	return bi
+function decodificar_partes(base,dec){
+	var p = dec.split(" ")
+	p[p.length-1] = parseInt([...p[p.length-1]].map(x=>parseInt(x,base)-10).join(""),base).toString(3)
+	var c = p.map(x=>x.toLowerCase())
+	return c
 }
-function bigint_a_b64(bi){
-	var arr = []
-	var base = 256n
-	while(bi>0){
-		arr.unshift( String.fromCharCode(+(""+(bi%base))) )
-		bi /= base
-	}
-	var s = arr.join("")
-	var b64 = btoa(s)
-	return b64
+function base64_a_meses(mabc,b64){
+	var dabc = abc_faltan(mabc)
+	var nabc = ordenar_vocales(dabc)
+	var base = nabc.length-1
+	var bi = b64_a_bigint(b64)
+	var cod = bigint_a_cod(nabc,bi)
+	var dec = decodificar(nabc,cod)
+	var partes = decodificar_partes(base,dec)
+	console.log(partes)
+	return partes
 }
-function meses_a_base64(a,b){
-	var codificado = codificar(a,b)
-	var bi = cod_a_bigint(codificado[2])
-	var b64 = bigint_a_b64(bi)
-	var nabc = abc_faltan(codificado[0])
-	return [nabc,b64]
-}
-var a = "121010110101"
-var b = "enero febrero marzo abril mayo junio julio agosto septiembre octubre noviembre diciembre domingo lunes martes miércoles jueves viernes sábado"
-console.log(meses_a_base64(a,b))
+var mabc = "HKQWXÁÉ "
+var b64 = "vS1UUlluGCqq2ZioYx3GehHWVC1O8rv8+gOvYC/viwgeLCo8g6W8o8DOwC6nrZefIoWUrOpTmr3Lnw6x4dQIBkZl+5lwd71J0jBEsp4qXbw2iDaz"
+var e = base64_a_meses(mabc,b64)
+console.log(e)
